6ecee411e45d55e7001d8cfd879220fc0db68069
k-kaz-git <k.kaz.nsy+web@gmail.com>
2019-02-21 11:41:25 +0900

    rebuilding site 2019年 2月 21日 木曜日 11:41:25

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ hugo
 cd public
 
 # Add changes to git.
-git add -A
+git add .
 
 # Commit changes.
 msg="rebuilding site `date`"
@@ -25,7 +25,7 @@ git push origin master
 cd ..
 
 # Add changes to git.
-git add -A
+git add .
 
 # Commit changes.
 msg="rebuilding site `date`"
